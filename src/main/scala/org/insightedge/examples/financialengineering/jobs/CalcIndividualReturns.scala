ed295ec63af790818a35497dc9d477e99b2bb2e3
Jason Nerothin <jasonnerothin@gmail.com>
2017-01-20 23:02:35 -0600

    Calculate IndividualReturns.

--- src/main/scala/org/insightedge/examples/financialengineering/jobs/CalcIndividualReturns.scala
+++ src/main/scala/org/insightedge/examples/financialengineering/jobs/CalcIndividualReturns.scala
@@ -2,12 +2,11 @@ package org.insightedge.examples.financialengineering.jobs
 
 import org.apache.spark.SparkContext
 import org.apache.spark.streaming.StreamingContext
-import org.insightedge.examples.financialengineering.{Settings, SpaceUsage, makeStreamingContext}
+import org.insightedge.examples.financialengineering.{Settings, makeStreamingContext}
 import org.insightedge.examples.financialengineering.model._
 import org.insightedge.spark.implicits.all._
 import InvestmentHelp.Help
 import TickDataHelp.Help
-import org.openspaces.core.GigaSpace
 
 import scala.collection.mutable
 
@@ -21,32 +20,35 @@ import scala.collection.mutable
   */
 class CalcIndividualReturns  {
 
-  val streamingCtx: StreamingContext =
+  private val streamingCtx: StreamingContext =
     makeStreamingContext(
       Settings.calcIndividualAppName,
       Settings.calcIndividualContextFrequencyMilliseconds
     )
+  private val sc = streamingCtx.sparkContext
 
   def main(args: Array[String]): Unit = {
 
     TickerSymbols.provideTickerSymbolForCalcIndividualReturn() match {
 
       case Some(tickerSymbol) =>
+
         val sym = tickerSymbol.abbreviation
-        val sc = streamingCtx.sparkContext
 
         val tdRdd = sc.gridSql[TickData](s"WHERE symbol = '$sym' AND processed = false")
+
+        val returns: mutable.LinkedList[InvestmentReturn] = mutable.LinkedList[InvestmentReturn]()
 
         val itr = tdRdd.map(td =>
           (monthAgoTick(td), td)
         ).collect().toIterator
 
-        val returns: mutable.LinkedList[InvestmentReturn] = mutable.LinkedList[InvestmentReturn]()
         while (itr.hasNext) {
           val n = itr.next
           val i = n._1.toInvestment(n._2)
           returns ++ List(InvestmentReturn(null, i.endDateMs, i.id, i.CAGR()))
         }
+
         streamingCtx.sparkContext.parallelize(returns).saveToGrid()
 
         TickerSymbols.returnSymbolFromCalcIndividualReturn(tickerSymbol)
@@ -74,6 +76,5 @@ class CalcIndividualReturns  {
       "ORDER BY timestampMs DESC")
     oldTick.first
   }
-
 
 }
